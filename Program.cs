21c1e5bda46e6cd13e259825dabb2346a9bc0271
Tiago <Tiago@DESKTOP-S4BLET0>
2023-10-31 18:50:04 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using RenameProject;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Insira o caminho do projeto novo: ");
+        var directory = Console.ReadLine();
+
+        Console.Write("Insira o nome de projeto do projeto antigo: ");
+        var oldName = Console.ReadLine();
+
+        Console.Write("Insira o nome de projeto do projeto novo: ");
+        var newName = Console.ReadLine();
+
+        if (directory == null)
+            throw new Exception("Insira o caminho do diretório do projeto");
+
+        Replace replace = new Replace();
+
+        replace.RenameFolderContent(directory, oldName, newName);
+    }
+}
---
+++ Replace.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace RenameProject
+{
+    public class Replace
+    {
+        public void RenameFolderContent(string path, string oldName, string newName)
+        {
+            DirectoryInfo d = new DirectoryInfo($"{path}");
+
+            FileInfo[] infos = d.GetFiles();
+            foreach (FileInfo f in infos)
+            {
+                File.Move(f.FullName, $"{path}\\{f.Name.Replace(oldName, newName)}");
+            }
+
+            string[] files = Directory.GetFiles(path);
+
+            foreach (string file in files)
+            {
+                string textFile = File.ReadAllText(file);
+                textFile = textFile.Replace(oldName, newName);
+                File.WriteAllText(file, textFile);
+            }
+
+            DirectoryInfo[] dirs = d.GetDirectories();
+            foreach (DirectoryInfo dir in dirs)
+            {
+                RenameFolderContent(dir.FullName, oldName, newName);
+                if(dir.Name.Contains(oldName))
+                {
+                    dir.MoveTo($"{path}\\{dir.Name.Replace(oldName, newName)}");
+                }
+            }
+
+        }
+    }
+}
